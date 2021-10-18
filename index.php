4982dd8d1f82d8e156b322c3c01a41e2a266535b
sofia-papa <papa.sofia@outlook.it>
2021-10-18 15:32:05 +0100

    database dischi

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+    require __DIR__ .'/db/dataBase.php'
+;?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP AJAX DISCHI</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <img src="" alt="spotify logo">
+        </header>
+
+        <div class="dischi-album">
+            
+        </div>
+    </div>
+    
+</body>
+</html>
