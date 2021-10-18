560abf3ecc3e8edf3659f95fc8e76ebe653a5df6
sofia-papa <papa.sofia@outlook.it>
2021-10-18 16:18:15 +0100

    foreach + css

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>PHP AJAX DISCHI</title>
 </head>
 <body>
@@ -18,9 +19,18 @@
             <img src="" alt="spotify logo">
         </header>
 
-        <div class="dischi-album">
-            
+        <div class="cd-album">
+            <?php foreach ($dischi as $album) { ?>
+                <div class="dischi-album">
+                    <img src = "<?php echo $album["poster"]; ?>"  >
+                    <h3> <?php echo $album["title"]; ?></h3>
+                    <p> <?php echo $album["author"]; ?></p>
+                    <p> <?php echo $album["year"] ; ?></p>
+                </div>
+            <?php ; } ?>
         </div>
+ 
+        
     </div>
     
 </body>
